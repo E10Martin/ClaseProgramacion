065917b0a426e9e4fb9f2a997753f2ea6ae63ea7
E10Martin <esteban.martin@cesjuanpablosegundo.es>
2024-10-01 09:49:51 +0200

    todos los ejer

--- T2/EjerciciosClase/src/Ejercicios.java
+++ T2/EjerciciosClase/src/Ejercicios.java
@@ -12,6 +12,7 @@ public class Ejercicios {
         //ejer11();
        //ejer12();
         //ejer13();
+        ejer15();
     }
     public static void ejer1(){
      final String nombre = "Luis González Sanz ";
@@ -64,7 +65,6 @@ public class Ejercicios {
         System.out.println("El resultado resto es "+resultado2);
 
     }
-
     public static void ejer4() {
 
         double bebidas = 1.25;
@@ -121,6 +121,116 @@ public class Ejercicios {
         float compraFinal = valor- compra;
         System.out.printf("Compra: %.2f\n", compraFinal);
         System.out.printf("IVA : %.2f", compra);
+
+    }
+    public static void ejer7(){
+        //1. Permítase introducir el valor del radio de una circuferencia con valores entre 0 y 100.
+        // Obténgase la longitud de la circunferencia (2πr) y el área del circulo (πr2) .(Circunferencia) NOTA El valor de PI se obtiene con Math.PI
+        //*ENTRADA/SALIDA*
+        //Escribe un radio entero: **15**
+        //Longitud de la circunferencia: 94.24777960769379
+        //Area de circulo: 706.8583470577034
+        System.out.println("Escribe un radio entero; ");
+        int radio = teclado.nextInt();
+        double longitudCircunferencia =  Math.PI*radio*2;
+        System.out.println("Longitud de la circunferencia: "+ longitudCircunferencia);
+        double areaCirculo =  Math.PI*(radio*radio);
+        System.out.println("Area de circulo: "+ areaCirculo);
+    }
+    public static void ejer8(){
+        //Hágase una aplicación que permita realizar conversiones de temperaturas entre grados
+        //centígrados, farenheit y kelvin (los resultados se muestran redondeados a dos
+        //decimales). (Temperaturas)
+        //Grados centigrados: **15,70*
+        //Farenheit: 60,26 Kelvin: 288,85
+        //Grados Farenheit: **45,32**
+        //Centigrados: 7,4 Kelvin: 280,55
+        //Grados Kelvin: **345,23**
+        //Centigrados: 72,08 Farenheit: 161,74
+        System.out.println("Grados centigrados; ");
+        float centigrados = teclado.nextFloat();
+        float calculoF = (9*centigrados)/5+32;
+        float calculoK = (float) (centigrados + 273.15);
+        System.out.printf("Farenheit: %.2f Kelvin: %.2f\n ",calculoF, calculoK);
+
+
+        System.out.println("Grados Farenheit: ");
+        float farenheit = teclado.nextFloat();
+        float calculoC1 = (5*(farenheit-32))/9;
+        float calculoK1 = (float)  (calculoC1 + 273.15);
+        System.out.printf("Centigrados: %.2f Kelvin: %.2f\n", calculoC1, calculoK1);
+
+
+        System.out.println("Grados Kelvin; ");
+        float kelvin = teclado.nextFloat();
+        float calculoC2 = (float) (kelvin - 273.15);
+        float calculoF2 = (9*calculoC2)/5+32;
+        System.out.printf("Centigrados: %.2f Farenheit: %.2f ",calculoC2, calculoF2);
+
+
+    }
+    public static void ejer9(){
+        //Número de bebidas (entre 0 y 20): **3**
+        //Número de bocadillos (entre 0 y 20): **5**
+        //Precio de cada bebida (entre 0,00 y 3,00): **1,20**
+        //Precio de cada bocadillo (entre 0,00 y 3,00): **2,05**
+        //Número de alumnos (entre 1 y 10): **5**
+        System.out.println("Numero de bebidas (entre 0 y 20):");
+        int numBebidas = teclado.nextInt();
+        System.out.println("Numero de bocatas (entre 0 y 20):");
+        int numBocatas = teclado.nextInt();
+        System.out.println("Precio de cada bebida (entre 0,00 y 3,00): ");
+        float precioBebida = teclado.nextFloat();
+        System.out.println("Precio de cada bebida (entre 0,00 y 3,00): ");
+        float precioBocatas = teclado.nextFloat();
+        System.out.println("Numero de alumnos (entre 1 y 10): ");
+        int numAlumnos = teclado.nextInt();
+        //10s alinea el texto a la izquierda con un ancho de 10 caracteres.
+
+
+
+
+        // Imprimir encabezados
+        System.out.printf("%-10s %-10s %-8s %-8s\n", "ARTICULO", "CANTIDAD", "PRECIO", "COSTE");
+        System.out.println("==========  =========  =======  =======");
+
+
+        // Imprimir datos
+        System.out.printf("%-10s %-10d %-8.2f %-8.2f\n", "Bebida",numBebidas, precioBebida, numBebidas * precioBebida);
+        System.out.printf("%-10s %-10d %-8.2f %-8.2f\n", "Bocadillo", numBocatas, precioBebida, numBocatas * precioBocatas);
+
+
+        // Imprimir total
+        System.out.println("                                =======");
+        System.out.printf("%-10s %28.2f\n", "TOTAL", (numBebidas *precioBebida) + (numBocatas * precioBocatas));
+        System.out.println("-------------------------------");
+
+
+    }
+    public static void ejer10(){
+        //Decenas de mil: **7**
+        //Unidades de mil: **9**
+        //Centenas: **0**
+        //Decenas: **5**
+        //Unidades: **0**
+        //Numero introducido: 79050
+        System.out.println("Decenas de mil: ");
+        int decenasMil = teclado.nextInt();
+        int decenasMil1 = 10000 * decenasMil;
+        System.out.println("Unidades de mil: ");
+        int unidadesMil = teclado.nextInt();
+        int unidadesMil1 = unidadesMil * 1000;
+        System.out.println("Centenas: ");
+        int centenas = teclado.nextInt();
+        int centenas1 = centenas * 100;
+        System.out.println("Decenas: ");
+        int decenas = teclado.nextInt();
+        int decenas1 = decenas * 10;
+        System.out.println("Unidades: ");
+        int unidades = teclado.nextInt();
+        int resultado =  decenasMil1+unidadesMil1+centenas1+decenas1+unidades;
+        System.out.println("Numero introducido es; "+resultado);
+
 
     }
     public static void ejer11(){
@@ -186,6 +296,24 @@ public class Ejercicios {
         System.out.println("El primero es superior al doble del segundo e inferior a 8: "+ comparar1);
         boolean comparar2 = n1 == n2 || (n1 - n2)<2;
         System.out.println("Son iguales o la diferencia entre el primero y el segundo es menor que 2: "+ comparar2);
+    }
+    public static void ejer15(){
+    //ENTERO: **21**
+        //Incrementar 5 unidades: 26
+        //Decrementar 3 unidades: 23
+        //Multiplicar por 10:
+        System.out.println("ENTERO: ");
+        int num = teclado.nextInt();
+        num +=5;
+        System.out.println("Incrementar 5: "+ num);
+        num -=3;
+        System.out.println("Decrementar 3 unidades: "+ num);
+        num *=10;
+        System.out.println("Multiplicar por 10: "+ num);
+        num /= 2;
+        System.out.println("Dividir entre dos: "+ num);
+
+
     }
 }
 
