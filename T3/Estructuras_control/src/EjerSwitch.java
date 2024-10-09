d8ef7b3a38e8705e645135f359b7483e8f044ef9
E10Martin <esteban.martin@cesjuanpablosegundo.es>
2024-10-09 14:23:29 +0200

    EJERCICIOS

--- T3/Estructuras_control/src/EjerSwitch.java
+++ T3/Estructuras_control/src/EjerSwitch.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class EjerSwitch {
     static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
         //ejer1();
         //ejer2();
-        ejer3();
+        //ejer3();
         //ejer4();
+        //ejer5();
+        //ejer7();
     }
-    public static void ejer1(){
+
+    public static void ejer1() {
         //Escribe un programa en Java que solicite al usuario ingresar un número del 1 al 7 y
         // muestre el día de la semana correspondiente utilizando una sentencia switch.
         System.out.println("Introduce un numero del 1 al 7;");
         int num = teclado.nextInt();
-        switch (num){
+        switch (num) {
             case 1:
                 System.out.println("Es lunes");
                 break;
@@ -39,17 +43,18 @@ public class EjerSwitch {
                 System.out.println("No me vaciles retra.");
         }
     }
-    public static void ejer2(){
+
+    public static void ejer2() {
         //Crea un programa que permita al usuario ingresar un carácter y
         // luego determine si es una vocal o una consonante utilizando una sentencia switch.
         System.out.println("Introduce un caracter");
         char palabra = teclado.next().charAt(0);
         switch (palabra) {
-            case 'a' ,'A':
-            case 'e' ,'E':
-            case 'i' ,'I':
-            case 'o' ,'O':
-            case 'u' ,'U':
+            case 'a', 'A':
+            case 'e', 'E':
+            case 'i', 'I':
+            case 'o', 'O':
+            case 'u', 'U':
                 System.out.println("Vocal");
                 break;
             default:
@@ -58,12 +63,13 @@ public class EjerSwitch {
         }
 
     }
-    public static void ejer3(){
+
+    public static void ejer3() {
         //Desarrolla un programa que reciba un número entero y, utilizando una sentencia switch, determine si es par o impar.
         System.out.println("Dame un numero entero;");
         int n1 = teclado.nextInt();
-        int n2 = n1%2;
-        switch (n2){
+        int n2 = n1 % 2;
+        switch (n2) {
             case 0:
                 System.out.println("Es par");
                 break;
@@ -71,12 +77,13 @@ public class EjerSwitch {
                 System.out.println("Es impar");
         }
     }
-    public static void ejer4(){
+
+    public static void ejer4() {
         //Realiza un programa que solicite al usuario ingresar un mes (como un número del 1 al 12)
         // y muestre la cantidad de días que tiene ese mes, considerando años no bisiestos.
         System.out.println("Ingresa un mes(con un numero del 1 al 12);");
         String mes = teclado.next();
-        switch (mes.toLowerCase()){
+        switch (mes.toLowerCase()) {
             case "marzo":
             case "abril":
             case "mayo":
@@ -95,6 +102,90 @@ public class EjerSwitch {
                 break;
             default:
                 System.out.println("Se serio anda");
+        }
+    }
+
+    public static void ejer5() {
+        System.out.println("Introduce un numero del 1 al 12");
+        int n1 = teclado.nextInt();
+        switch (n1) {
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Noviembre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:
+                System.out.println("Tete del 1 al 12 te he dicho retrasao.");
+        }
+    }
+
+    public static void ejer7() {
+        System.out.println("Introduce un numero del 1 al 5");
+        int n1 = teclado.nextInt();
+        switch (n1) {
+            case 0, 1, 2:
+                System.out.println("Suspenso");
+                break;
+            case 3:
+                System.out.println("Aprobao");
+                break;
+            case 4:
+                System.out.println("NOtable");
+                break;
+            case 5:
+                System.out.println("Soobresaliente");
+                break;
+            default:
+                System.out.println("Del 1 al 5 aunqu seguro que has sacado un 0.");
+
+
+        }
+
+    }
+
+    public static void ejer8() {
+        System.out.println("Introduce un caracter (+, -, *, /)");
+        char caracter = teclado.next().charAt(0);
+        System.out.println("Introduce un numero");
+        int n2 = teclado.nextInt();
+        System.out.println("Introduce un numero");
+        int n3 = teclado.nextInt();
+
+        switch (caracter) {
+            case +:
+
+
         }
     }
 
