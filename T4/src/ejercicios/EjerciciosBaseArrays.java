711ab5be3e9973dadb92f162211c049a6b861b6d
E10Martin <esteban.martin@cesjuanpablosegundo.es>
2024-11-05 13:24:07 +0100

    ejer

--- T4/src/ejercicios/EjerciciosBaseArrays.java
+++ T4/src/ejercicios/EjerciciosBaseArrays.java
@@ -8,17 +8,25 @@ public class EjerciciosBaseArrays {
     static Random aleatorio = new Random();
 
     public static void main(String[] args) {
+        //ejer1();
         ejer2();
     }
-    static void ejer2(){
+     public static void ejer1(){
         //Realiza un programa que pida 10 números por teclado y que los almacene en un array.
         // A continuación se mostrará el contenido de ese array junto al índice (0 – 9)
 
         int numero[]= new int [10];
-        for (int i = 0; i < numero.length ; i++) {
+        for (int i = 0; i < 10 ; i++) {
             System.out.println("Introduce un numero: ");
             numero[i] = teclado.nextInt();
-            System.out.println("El numero es; "+ numero[i]);
+        }
+        for (int i = 0; i < numero.length; i++) {
+            System.out.printf("El numero en la posicion %d es %d\n ", i+1,numero[i]);
         }
     }
+
+    public static void ejer2(){
+
+    }
+
 }
